ec7502bc0604aab9a1bda50ad67d45cc7a9d2943
Jayant Bhawal <bhawal.jayant@gmail.com>
2021-05-04 20:23:12 +0530

    Add SSR support with HBS

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fetch = require("node-fetch");
 const express = require("express");
 const path = require("path");
 
@@ -5,15 +6,22 @@ const app = express();
 
 const port = process.env.port || 3000;
 
-app.use(express.static("dist"));
+app.set("view engine", "html");
+app.engine("html", require("hbs").__express);
+app.set("views", __dirname + "/dist");
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/dist/index.html"));
+app.get("/", async (req, res) => {
+  const status = await fetch(`http://localhost:${port}/status`).then(
+    (r) => r.status
+  );
+  res.render("index", { status });
 });
 
 app.get("/status", (req, res) => {
   res.sendStatus(200);
 });
+
+app.use(express.static("dist"));
 
 app.listen(port, "0.0.0.0", () => {
   console.log(`[Express + Parcel] App listening at http://localhost:${port}`);
--- public/main.js
+++ public/main.js
@@ -1,8 +1,6 @@
 const getBackendStatus = async () => {
   const response = await fetch("/status").then((r) => r.status);
-  const el = document.getElementById("backend-status");
-  if (!el) return;
-  el.innerText = response;
+  console.log("Backend status from client: 200");
 };
 
 window.onload = () => {
